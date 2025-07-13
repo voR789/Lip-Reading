5fa15b12e71254c0122ed4d64efaf4b87a78da8d
Ryan <ryanvo2020@gmail.com>
2025-07-12 19:49:04 -0500

    lip detection metrics planned

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,4 @@
 from .mediapipe_utils import *
 from .opencv_utils import *
 
-__all__ = [get_landmarks,convert_landmarks_to_coordinates,get_bounding_box,load_video]
+__all__ = [get_landmarks,convert_landmarks_to_coordinates,get_bounding_box,load_video,landmark_dist,area_of_points]
--- util/mediapipe_utils.py
+++ util/mediapipe_utils.py
@@ -1,4 +1,5 @@
 import mediapipe as mp
+import numpy as np
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
@@ -27,3 +28,19 @@ def get_bounding_box(coords, image_shape, padding = 10):
     x_max = min(max(x) + padding , w)
     y_max = min(max(y) + padding, h)
     return x_min, y_min, x_max, y_max
+
+def landmark_dist(l1, l2):
+    p1 = np.array(l1)
+    p2 = np.array(l2)
+    return np.linalg.norm(p2 - p1)
+
+def area_of_points(arr, coords):
+    # Gaussian Area Formula - "Shoelace Method"
+    area = 0
+    size = len(arr)
+    for i in range(size - 1):
+        area += coords[arr[i]][0]*coords[arr[i+1]][1]
+        area -= coords[arr[i + 1]][0]*coords[arr[i]][1]
+    area += coords[arr[-1]][0]*coords[arr[0]][1]
+    area -= coords[arr[0]][0]*coords[arr[-1]][1]
+    return area
